b8b4aa0010ec3de90eb85d7c0550f37f5168f788
Frank Hart <frankhart010@gmail.com>
2023-01-05 14:23:53 +1000

    All drawing now done in world space only

--- src/xnIScene.h
+++ src/xnIScene.h
@@ -17,12 +17,12 @@ namespace xn
 
     virtual ~IScene() {}
 
-    virtual void AddLine(xn::seg const &, float thickness, xn::Colour clr, uint32_t flags, uint32_t layer, xn::mat33 T_Model_World = xn::mat33()) = 0;
-    virtual void AddLineGroup(std::vector<xn::seg> const &, float thickness, xn::Colour clr, uint32_t flags, uint32_t layer, xn::mat33 T_Model_World = xn::mat33()) = 0;
-    virtual void AddFilledCircle(xn::vec2 const &centre, float radius, xn::Colour clr, uint32_t flags, uint32_t layer, xn::mat33 T_Model_World = xn::mat33()) = 0;
-    virtual void AddFilledCircleGroup(std::vector<xn::vec2> const &centres, float radius, uint32_t flags, uint32_t layer, xn::Colour clr, xn::mat33 T_Model_World = xn::mat33()) = 0;
-    virtual void AddPolygon(xn::DgPolygon const &, float thickness, xn::Colour clr, uint32_t flags, uint32_t layer, xn::mat33 T_Model_World = xn::mat33()) = 0;
-    virtual void AddFilledPolygon(xn::DgPolygon const &, xn::Colour clr, uint32_t flags, uint32_t layer, xn::mat33 T_Model_World = xn::mat33()) = 0;
+    virtual void AddLine(xn::seg const &, float thickness, xn::Colour clr, uint32_t flags, uint32_t layer = 0) = 0;
+    virtual void AddLineGroup(std::vector<xn::seg> const &, float thickness, xn::Colour clr, uint32_t flags, uint32_t layer = 0) = 0;
+    virtual void AddFilledCircle(xn::vec2 const &centre, float radius, xn::Colour clr, uint32_t flags, uint32_t layer = 0) = 0;
+    virtual void AddFilledCircleGroup(std::vector<xn::vec2> const &centres, float radius, xn::Colour clr, uint32_t flags, uint32_t layer = 0) = 0;
+    virtual void AddPolygon(xn::DgPolygon const &, float thickness, xn::Colour clr, uint32_t flags, uint32_t layer = 0) = 0;
+    virtual void AddFilledPolygon(xn::DgPolygon const &, xn::Colour clr, uint32_t flags, uint32_t layer = 0) = 0;
   };
 }
 
