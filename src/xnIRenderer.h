9db273e895a558d0341d86b029f506934dd5cd66
Frank Hart <frankhart010@gmail.com>
2023-02-22 04:41:49 +1000

    Added new rendering functions

--- src/xnIRenderer.h
+++ src/xnIRenderer.h
@@ -19,8 +19,10 @@ namespace xn
 
     virtual void DrawLine(seg const &, float thickness, Colour clr, uint32_t flags) = 0;
     virtual void DrawLineGroup(std::vector<seg> const &, float thickness, Colour clr, uint32_t flags) = 0;
-    virtual void DrawFilledCircle(vec2 const &centre, float radius, Colour clr, uint32_t flags) = 0;
-    virtual void DrawFilledCircleGroup(std::vector<vec2> const &centres, float radius, Colour clr, uint32_t flags) = 0;
+    virtual void DrawCircle(vec2 const &centre, float size, float thickness, Colour clr, uint32_t flags) = 0;
+    virtual void DrawCircleGroup(std::vector<vec2> const &centres, float size, float thickness, Colour clr, uint32_t flags) = 0;
+    virtual void DrawFilledCircle(vec2 const &centre, float size, Colour clr, uint32_t flags) = 0;
+    virtual void DrawFilledCircleGroup(std::vector<vec2> const &centres, float size, Colour clr, uint32_t flags) = 0;
     virtual void DrawPolygon(DgPolygon const &, float thickness, Colour clr, uint32_t flags) = 0;
     virtual void DrawPolygon(std::vector<vec2> const &vertices, float thickness, Colour clr, uint32_t flags) = 0;
     virtual void DrawFilledPolygon(DgPolygon const &, Colour clr, uint32_t flags) = 0;
